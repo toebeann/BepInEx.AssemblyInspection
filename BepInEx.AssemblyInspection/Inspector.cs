c234fa5024130d0995495748077f6746b23fdab2
Tobey Blaber <contact@tobey.me>
2023-01-26 00:28:58 +0000

    Initial commit

---
+++ BepInEx.AssemblyInspection/Inspector.cs
@@ -0,0 +1,67 @@
+﻿using Mono.Cecil;
+using Mono.Cecil.Rocks;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace BepInEx.AssemblyInspection
+{
+    public class Inspector
+    {
+        [JsonIgnore]
+        public IEnumerable<TypeDefinition> PluginTypes;
+        [JsonIgnore]
+        public IEnumerable<TypeDefinition> PatcherTypes;
+
+        public IEnumerable<string> Plugins => PluginTypes.Select(t => t.FullName);
+        public IEnumerable<string> Patchers => PatcherTypes.Select(t => t.FullName);
+        public bool HasPlugins;
+        public bool HasPatchers;
+
+        public Inspector(string filePath, IEnumerable<string> searchDirectories = null)
+        {
+            if (searchDirectories is null)
+            {
+                searchDirectories = Enumerable.Empty<string>();
+            }
+
+            using (var resolver = new DefaultAssemblyResolver())
+            {
+                foreach (var dir in Directory.GetDirectories(RuntimeEnvironment.GetRuntimeDirectory(), "*", SearchOption.AllDirectories))
+                {
+                    resolver.AddSearchDirectory(dir);
+                }
+                foreach (var dir in searchDirectories)
+                {
+                    resolver.AddSearchDirectory(dir);
+                }
+                resolver.AddSearchDirectory(Path.GetDirectoryName(filePath));
+
+                using (var assemblyDefinition = AssemblyDefinition.ReadAssembly(filePath, new ReaderParameters { AssemblyResolver = resolver }))
+                {
+                    PluginTypes = GetPluginTypes(assemblyDefinition);
+                    HasPlugins = PluginTypes.Any();
+                    PatcherTypes = GetPatcherTypes(assemblyDefinition);
+                    HasPatchers = PatcherTypes.Any();
+                }
+            }
+        }
+
+        public static IEnumerable<TypeDefinition> GetPluginTypes(AssemblyDefinition assemblyDefinition) =>
+            assemblyDefinition.Modules
+                .SelectMany(m => m.GetAllTypes())
+                .Where(t => t.CustomAttributes.Any(a => a.AttributeType.FullName == typeof(BepInPlugin).FullName));
+
+        public static IEnumerable<TypeDefinition> GetPatcherTypes(AssemblyDefinition assemblyDefinition) =>
+            assemblyDefinition.Modules
+                .SelectMany(m => m.GetAllTypes())
+                .Where(t => t.Properties.Any(p => p.Name == "TargetDLLs"
+                        && p.PropertyType.FullName == "System.Collections.Generic.IEnumerable`1<System.String>")
+                    && t.Methods.Any(m => m.Name == "Patch"
+                        && m.ReturnType.FullName == typeof(void).FullName
+                        && m.HasParameters
+                        && m.Parameters.SingleOrDefault()?.ParameterType.FullName == typeof(AssemblyDefinition).FullName));
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,66 @@
+﻿using Fclp;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BepInEx.AssemblyInspection
+{
+    internal class Program
+    {
+        public class Arguments
+        {
+            [Flags]
+            public enum BepInExType
+            {
+                Plugins = 1,
+                Patchers = 2,
+                All = Plugins | Patchers
+            }
+
+            public string FilePath { get; set; }
+            public BepInExType Types { get; set; }
+            public List<string> SearchDirectories { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var p = new FluentCommandLineParser<Arguments>();
+
+            p.Setup(arg => arg.FilePath)
+                .As('f', "file-path")
+                .Required();
+
+            p.Setup(arg => arg.Types)
+                .As('t', "types")
+                .SetDefault(Arguments.BepInExType.All);
+
+            p.Setup(arg => arg.SearchDirectories)
+                .As('s', "search-directories")
+                .SetDefault(new List<string>());
+
+            var parsed = p.Parse(args);
+
+            if (parsed.HasErrors)
+            {
+                Console.WriteLine(parsed.ErrorText);
+                Environment.Exit(1);
+                return;
+            }
+
+            var inspector = new Inspector(p.Object.FilePath, p.Object.SearchDirectories);
+            switch (p.Object.Types)
+            {
+                case Arguments.BepInExType.All:
+                    Console.WriteLine(JsonConvert.SerializeObject(inspector));
+                    break;
+                case Arguments.BepInExType.Plugins:
+                    Console.WriteLine(JsonConvert.SerializeObject(inspector.HasPlugins));
+                    break;
+                case Arguments.BepInExType.Patchers:
+                    Console.WriteLine(JsonConvert.SerializeObject(inspector.HasPatchers));
+                    break;
+            }
+        }
+    }
+}
